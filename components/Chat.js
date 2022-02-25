394055b5540b6a8f9325d2c97b2e129ce6478bc3
rusevaIvelina <iva.ruseva14@gmail.com>
2022-02-25 15:43:36 -0500

    added additional features

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default class Chat extends React.Component {
     render() {
         let name = this.props.route.params.name;
         this.props.navigation.setOptions({ title: name });
 
+        const { bgColor } = this.props.route.params;
+
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Button
-                  title='Go to Start'
-                  onPress={() => this.props.navigation.navigate('Start')}
-                />
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: bgColor
+            }}>
+                <Text>Hello Chat</Text>
             </View>
         );
     }
 }
+
